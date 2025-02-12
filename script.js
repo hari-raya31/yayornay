36926ff85a33ea9ef28266a47575fb341e8a3f1d
hari-raya31 <amalia311003@gmail.com>
2025-02-12 22:20:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,14 +54,14 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Iyakah?",
+    "Beneran??",
+    "dem",
+    "walach",
+    ":D",
+    "Yakin..",
+    "hmmm",
+    "naurrr",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
